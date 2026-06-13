20969de6055d6cbf18961551486dacf8f2d0ab97
Raihan Adam <hallwack.id@gmail.com>
2026-06-13 20:48:08 +0700

    fix: fixing zennotes so user can user zen cli

--- pkgs/zennotes/default.nix
+++ pkgs/zennotes/default.nix
@@ -3,7 +3,20 @@
   lib,
   appimageTools,
   fetchurl,
+  glib,
+  gtk3,
+  cairo,
+  pango,
+  nss,
+  nspr,
+  alsa-lib,
+  cups,
+  libuuid,
+  libnotify,
+  atk,
+  at-spi2-atk,
 }:
+
 let
   pname = "zennotes";
   version = "2.3.0";
@@ -16,40 +29,81 @@ let
   };
 
   system =
-    architectures.${stdenv.hostPlatform.system}
-      or (throw "Unsupported system: ${stdenv.hostPlatform.system}. Supported systems are: ${lib.attrNames architectures}");
+    architectures.${stdenv.hostPlatform.system} or (throw ''
+      Unsupported system: ${stdenv.hostPlatform.system}
+      Supported systems: ${lib.concatStringsSep ", " (lib.attrNames architectures)}
+    '');
 
-  src =
-    let
-      inherit (system) arch hash;
-    in
-    fetchurl {
-      url = "https://github.com/ZenNotes/zennotes/releases/download/v${version}/ZenNotes-${version}-${arch}.AppImage";
-      inherit hash;
-    };
+  src = fetchurl {
+    url = "https://github.com/ZenNotes/zennotes/releases/download/v${version}/ZenNotes-${version}-${system.arch}.AppImage";
+    inherit (system) hash;
+  };
 
   appImageContents = appimageTools.extractType2 {
     inherit pname version src;
   };
+
 in
 appimageTools.wrapType2 {
   inherit pname version src;
 
-  extraPkgs = pkgs: with pkgs; [
+  extraPkgs = pkgs: [
     glib
+    gtk3
+    atk
+    at-spi2-atk
+    cairo
+    pango
+    nss
+    nspr
+    alsa-lib
+    cups
+    libuuid
+    libnotify
   ];
 
   extraInstallCommands = ''
+    #
+    # Desktop entry
+    #
     mkdir -p $out/share/applications
 
-    cp ${appImageContents}/*.desktop $out/share/applications/${pname}.desktop
+    cp ${appImageContents}/ZenNotes.desktop \
+      $out/share/applications/${pname}.desktop
 
     substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace "Exec=AppRun" "Exec=${pname}"
+      --replace-fail "Exec=AppRun" "Exec=${pname}" \
+      --replace "Comment=" "Comment=Keyboard-first local Markdown notes"
 
-    sed -i 's/^Comment=.*/Comment=Keyboard-first local Markdown notes/' $out/share/applications/${pname}.desktop
+    #
+    # Icon
+    #
+    mkdir -p $out/share/icons/hicolor/512x512/apps
 
-    cp -r ${appImageContents}/usr/share/icons $out/share || true
+    cp ${appImageContents}/ZenNotes.png \
+      $out/share/icons/hicolor/512x512/apps/${pname}.png
+
+    #
+    # CLI launcher
+    #
+    # Upstream ships resources/zen which directly launches the
+    # Electron binary. That bypasses the FHS environment generated
+    # by wrapType2 and causes missing library errors on NixOS.
+    #
+    # Instead, execute the wrapped zennotes launcher and point it
+    # at the bundled cli.js.
+    #
+    cat > $out/bin/zen <<EOF
+    #!${stdenv.shell}
+
+    export ELECTRON_RUN_AS_NODE=1
+
+    exec "$out/bin/${pname}" \
+      ${appImageContents}/resources/cli.js \
+      "\$@"
+    EOF
+
+        chmod +x $out/bin/zen
   '';
 
   meta = with lib; {
@@ -57,6 +111,8 @@ appimageTools.wrapType2 {
     homepage = "https://zennotes.org/";
     license = licenses.mit;
     mainProgram = pname;
-    platforms = builtins.attrNames architectures;
+    platforms = [
+      "x86_64-linux"
+    ];
   };
 }
